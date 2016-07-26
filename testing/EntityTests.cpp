e4bc0eb0334e043d7f66bf5ad023847a0190ae2e
mstehula <mstehula@gmail.com>
2016-07-26 16:43:49 +0000

    Fleshed out some testing. Should probably move from this to Unit testing next chance I get

--- testing/EntityTests.cpp
+++ testing/EntityTests.cpp
@@ -8,4 +8,31 @@ int main(int argc, char** argv)
   printf("EM: \t%d\n",sizeof(ecse::EntityManager));
   printf("EId: \t%d\n",sizeof(ecse::Entity::Id));
   printf("E: \t%d\n",sizeof(ecse::Entity));
+
+  ecse::EntityManager em;
+
+  ecse::Entity entity1 = em.Create();
+  ecse::Entity entity2 = em.Create();
+
+  printf("E1 %d\n", entity1.GetId().Get());
+  printf("E1,I %d\n", entity1.GetId().Index());
+  printf("E1,V %d\n", entity1.GetId().Version());
+  printf("E2 %d\n", entity2.GetId().Get());
+  printf("E2,I %d\n", entity2.GetId().Index());
+  printf("E2,V %d\n", entity2.GetId().Version());
+
+  printf("E1 valid: %d\n", em.Validate(entity1.GetId()));
+  printf("E2 valid: %d\n", em.Validate(entity2.GetId()));
+
+  em.Destroy(entity1.GetId());
+  em.Destroy(entity2.GetId());
+
+  printf("E1 valid: %d\n", em.Validate(entity1.GetId()));
+  printf("E2 valid: %d\n", em.Validate(entity2.GetId()));
+
+  entity1 = em.Create();
+  printf("E1 %d\n", entity1.GetId().Get());
+  printf("E1,I %d\n", entity1.GetId().Index());
+  printf("E1,V %d\n", entity1.GetId().Version());
+  
 }
