7e65d3f69cfa1d65a8f5f3f15864dd31d1a12d0d
mstehula <mstehula@gmail.com>
2016-08-18 08:27:04 +0000

    Added new event tests to test compatability between the two. One inherits the other, and there is a callback for each. The callback only registeres with the one it's supposed to. Also the inherited values are good to go through both, as long as you only call the ones that are registered. This is polymorphism

--- testing/EventTests.cpp
+++ testing/EventTests.cpp
@@ -4,23 +4,32 @@
 
 #include "ecse/Event.hpp"
 
-struct TestEvent : public ecse::Event
+struct TestEvent1 : public ecse::Event
 {
+public:
+  int id;
+};
 
+struct TestEvent2 : public TestEvent1
+{
+public:
+  int x, y, z;
 };
 
 class Listener
 {
 public:
 
-  static void Callback1(TestEvent event)
+  static void Callback1(TestEvent1 event)
   {
     printf("Callback 1 is here!\n");
+    printf("%d\n", event.id);
   }
 
-  static void Callback2(TestEvent event)
+  static void Callback2(TestEvent2 event)
   {
     printf("Callback 2 is here!\n");
+    printf("%d %d %d %d\n", event.id, event.x, event.y, event.z);
   }
 
 };
@@ -29,11 +38,21 @@ int main()
 {
   ecse::EventManager event_manager;
   Listener listener_;
-  TestEvent event;
+  TestEvent1 event1;
+  event1.id = 1;
 
-  event_manager.Subscribe<TestEvent>(listener_.Callback1);
-  event_manager.Subscribe<TestEvent>(listener_.Callback2);
-  printf("%s\n", "Callbacks registered!");
-  event_manager.Fire(event);
-  printf("%s\n", "Callback complete!");
+  TestEvent2 event2;
+  event2.id = 2;
+  event2.x = 1;
+  event2.y = 100;
+  event2.z = 120;
+
+  printf("Test Event 1\n");
+  event_manager.Subscribe<TestEvent1>(listener_.Callback1);
+  event_manager.Fire(event1);
+  event_manager.Fire(event2);
+
+  printf("Test Event 2\n");
+  event_manager.Subscribe<TestEvent2>(listener_.Callback2);
+  event_manager.Fire(event2);
 }
